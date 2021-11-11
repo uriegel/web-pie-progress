3f7706e3227b902dd34ae417fd1c42ea58cc050c
uriegel <uriegel@hotmail.de>
2021-11-11 18:21:48 +0100

    Webserver test html

---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UwebServer;
+using UwebServer.Routes;
+
+var port = 9885;
+
+var routeWebSite = new Static() { FilePath = "." };
+var server = new Server(new()
+{
+    Port = port,
+    Routes = new[] { routeWebSite },
+});
+server.Start();
+Console.WriteLine($"Running test server on http://localhost:{port}/test/index.html");
+Console.ReadLine();
+
+
