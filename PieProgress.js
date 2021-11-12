ba5c01ae571e7fcbff8194d8ecd7dc447ff33456
uriegel <uriegel@hotmail.de>
2021-11-12 08:27:43 +0100

    svg

--- PieProgress.js
+++ PieProgress.js
@@ -29,11 +29,27 @@ export class PieProgress extends HTMLElement {
                     border-radius: 0 100% 100% 0 / 50%;
                     background-color: inherit;
                     transform-origin: left;
-                    transform: rotate(35deg);
+                    transform: rotate(140deg);
+                }
+                svg {
+                    aspect-ratio: 1;
+                    height: 100%;
+                    transform: rotate(-90deg);
+                    border-radius: 50%;
+                    background: var(--pieprogress-background-color);
+                }
+                circle {
+                    fill: var(--pieprogress-background-color);
+                    stroke: var(--pieprogress-color);
+                    stroke-width: 32;
+                    stroke-dasharray: 40 100;
                 }
             </style>
             
             <div id=pie></div>
+            <svg id=pie2 viewBox="0 0 32 32">
+                <circle r="16" cx="16" cy="16"/>
+            </svg> 
         `
         this.shadowRoot.appendChild(template.content.cloneNode(true))
     }
