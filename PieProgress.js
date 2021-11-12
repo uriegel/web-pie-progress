18008bebb4e486fa525ba13ba7b563b1b9387bb5
uriegel <uriegel@hotmail.de>
2021-11-12 18:43:04 +0100

    Finished

--- PieProgress.js
+++ PieProgress.js
@@ -21,21 +21,42 @@ export class PieProgress extends HTMLElement {
                 #background {
                     fill: var(--pieprogress-background-color);
                 }
-                #pi {
+                #progress-circle {
                     fill: transparent;
                     stroke: var(--pieprogress-color);
                     stroke-width: 50;
-                    stroke-dasharray: calc(45 * 3.14159 *50 / 100) calc(3.14159 *50);
+                    stroke-dasharray: 0 calc(3.14159 *50);
                     transform: rotate(-90deg) translate(-100px)
                 }
             </style>
             
             <svg id=pie viewBox="0 0 100 100">
                 <circle id="background" r="50" cx="50" cy="50"/>
-                <circle id="pi" r="25" cx="50" cy="50"/>
+                <circle id="progress-circle" r="25" cx="50" cy="50"/>
             </svg> 
         `
         this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+        this.progressCircle = this.shadowRoot.getElementById("progress-circle")
+    }
+
+    static get observedAttributes() {
+        return ['progress']
+    }
+    
+    attributeChangedCallback(attributeName, oldValue, newValue) {
+        switch (attributeName) {
+            case "progress":
+                if (oldValue != newValue)
+                    this.setProgress(newValue)
+                break
+        }
+    }
+
+    setProgress(progress) {
+        if (progress < 0 || progress > 100)
+            progress = 100
+        this.progressCircle.style.strokeDasharray = `calc(${progress} * 3.14159 *50 / 100) calc(3.14159 *50)`
     }
 }
 
--- test/index.js
+++ test/index.js
@@ -1,1 +1,9 @@
 import '../index.js'
+
+const progress = document.getElementById("progress")
+
+let value = 0.0
+
+setInterval(() => {
+    progress.setAttribute("progress", value++ / 10.0)
+}, 30)
